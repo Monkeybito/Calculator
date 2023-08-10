37c099369e1ec2d504e41baa24a85799e4d6d558
Monkeybito <139124353+Monkeybito@users.noreply.github.com>
2023-08-10 14:00:30 +0200

    I created four functions to add, subtract, multiply and divide two variables; furthermore I added variables that will hold the selected value later

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a,b){
+    return a + b
+}
+
+function subtract(a,b){
+    return a - b
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function divide(a,b){
+    return a / b 
+}
+
+let number1 = 1
+let number2 = 1
+let operator = +
+
+
+Create a new function operate that takes an operator and 2 numbers and then 
+calls one of the above functions on the numbers.
