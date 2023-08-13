08e9a787599773b12f0ce4a7555238f0305d6844
Monkeybito <139124353+Monkeybito@users.noreply.github.com>
2023-08-13 16:37:15 +0200

    I added the adequate Events to the Eventlistener, it is now able to handle simple operations like + etc. and store the old value in oldnumber and empty up the newnumber variable

--- script.js
+++ script.js
@@ -14,9 +14,6 @@ function divide(a,b){
     return a / b 
 }
 
-let number1 = 1
-let number2 = 1
-let sign = "+"
 
 function operator(num1, sign, num2){
     if (sign === "+"){
@@ -33,8 +30,46 @@ function operator(num1, sign, num2){
     }
 }
 
+
+let oldnumber = ""
+let currentnumber = "" 
+let sign = ""
+let counter = 0
+const reg = new RegExp('^[0-9]+$');
+
+
+const display = document.querySelector(".display");
+var numbers = document.createTextNode("")
+display.appendChild(numbers)
+
 const buttons = document.querySelectorAll(".button");
 buttons.forEach(button => button.addEventListener("click", function(e){
-    let content = button.textContent
-    console.log(content)
+    if (button.textContent === "Clear"){
+        display.textContent = ""
+        counter = 0
+        return
+    }
+    else if (button.textContent != "+" && button.textContent != "-" && button.textContent != "*" && button.textContent != "/") {
+        display.textContent += button.textContent
+        currentnumber += button.textContent
+    }
+    else if (button.textContent === "+" || "-" || "*" || "/"){
+        if (counter === 0){
+            counter = 1
+            oldnumber = currentnumber
+            currentnumber = ""
+            sign = button.textContent
+            display.textContent += button.textContent
+            return
+        }
+        else if (counter === 1){
+            oldnumber = operator(parseInt(oldnumber), sign, parseInt(currentnumber))
+            currentnumber = ""
+            display.textContent = oldnumber 
+            display.textContent += button.textContent
+            sign = button.textContent
+        }
+    }
+   
 }))
+
